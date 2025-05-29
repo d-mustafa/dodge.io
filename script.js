a78d060f6b0487200d1237cd08b509d289c066f5
Divine <160066288+d-mustafa@users.noreply.github.com>
2025-05-29 15:23:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -12,75 +12,12 @@ let aPressed = false;
 let sPressed = false;
 let dPressed = false;
 let shiftPressed = 1;
-function recordKeyDown(event) {
-    if (event.code == "KeyW") {
-        wPressed = true;
-    }
-    if (event.code == "KeyA") {
-        aPressed = true;
-    }
-    if (event.code == "KeyS") {
-        sPressed = true;
-    }
-    if (event.code == "KeyD") {
-        dPressed = true;
-    }
-    if (event.code == "ShiftLeft" || event.code == "ShiftRight") {
-        if (keyboardMovementOn) {
-            shiftPressed = 0.5;
-        } else if (mouseMovementOn) {
-            shiftPressed = 0.5;
-        }
-    }
-
-    if (wPressed || aPressed || sPressed || dPressed) {
-        keyboardMovementOn = true;
-    }
-}
-function recordKeyUp(event) {
-    if (event.code == "KeyW") {
-        wPressed = false;
-    }
-    if (event.code == "KeyA") {
-        aPressed = false;
-    }
-    if (event.code == "KeyS") {
-        sPressed = false;
-    }
-    if (event.code == "KeyD") {
-        dPressed = false;
-    }
-    if (event.code == "ShiftLeft" || event.code == "ShiftRight") {
-        shiftPressed = 1;
-    }
-
-    if (!wPressed && !aPressed && !sPressed && !dPressed) {
-        keyboardMovementOn = false;
-    }
-}
 
 // Mouse
 document.addEventListener("click", recordMouseClicked)
 let mouseMovementOn = false;
 let mouseOverRestartButton = false;
 let mouseOverPlayButton = false;
-function recordMouseClicked() {
-    if (gameState == "gameOn" || gameState == "startScreen") {
-        if (mouseMovementOn) {
-            mouseMovementOn = false
-        } else if (!mouseMovementOn) {
-            mouseMovementOn = true;
-        }  
-    }
-    if (gameState == "startScreen" && mouseOverPlayButton) {
-        restartGame()
-        mouseOverPlayButton = false;
-    }
-    if (gameState == "gameOver" && mouseOverRestartButton) {
-        gameState = "startScreen"
-        mouseOverPlayButton = false;
-    }
-}
 
 cnv.addEventListener('mousemove', mousemoveHandler);
 let mouseX;
@@ -97,15 +34,26 @@ let player = {
     y: cnv.height/2,
     radius: 15,
     speed: 2.5,
+    color: "rgb(255, 0, 0)",
+    dash: 0.5,
 }
 let allEnemies = []
 
 // Time
+let now = Date.now()
 let time = 0;
 let score = 0;
 let highscore = 0;
 let highscoreColor = "rgb(87, 87, 87)";
 let enemySpawnTime = 200;
+
+// Abilities
+let dash = {
+    usable: true,
+    activated: false,
+    cooldown: 1000, // cooldowns are in milliseconds
+    lastUsed: 0,
+}
 
 requestAnimationFrame(draw)
 
@@ -117,11 +65,9 @@ function draw() {
         drawPlayer();
         drawStartScreen();
 
-        if (keyboardMovementOn) {
-            keyboardControls();
-        } else {
-            mouseMovement();
-        }
+        keyboardControls();
+        mouseMovement();
+        abilities();
     }
     else if (gameState == "gameOn") {
         drawScore();
@@ -129,17 +75,15 @@ function draw() {
         drawEnemies();
         spawnEnemyPeriodically();
 
-        if (keyboardMovementOn) {
-            keyboardControls();
-        } else {
-            mouseMovement();
-        }
+        keyboardControls();
+        mouseMovement();
         
         moveEnemies();
         collisions();
+
+        abilities();
     }
     else if (gameState == "gameOver") {
-        drawScore();
         drawPlayer();
         drawEnemies();
         drawGameOver();
@@ -149,314 +93,3 @@ function draw() {
 }
 
 draw()
-
-function drawStartScreen() {
-    // TITLE CARD //
-    const titleGrad = ctx.createLinearGradient(250, 50, 550, 150)
-    const titleGrad2 = ctx.createLinearGradient(250, 150, 550, 50)
-
-    let mouseOverTitle = (mouseX > 250 && mouseX < 550) && (mouseY > 50 && mouseY < 150);
-    if (mouseOverTitle) {
-        titleGrad.addColorStop(0, "rgb(114, 114, 114)");
-        titleGrad.addColorStop(1, "rgb(255, 255, 255)");
-
-        titleGrad2.addColorStop(0, "rgb(255, 255, 255)");
-        titleGrad2.addColorStop(1, "rgb(114, 114, 114)");
-    } else {
-        titleGrad.addColorStop(0, "rgb(255, 255, 255)");
-        titleGrad.addColorStop(1, "rgb(114, 114, 114)");
-
-        titleGrad2.addColorStop(0, "rgb(114, 114, 114)");
-        titleGrad2.addColorStop(1, "rgb(255, 255, 255)");
-    }
-
-    ctx.fillStyle = titleGrad;
-    ctx.fillRect(250, 50, 300, 100)
-    ctx.strokeStyle = titleGrad2;
-    ctx.beginPath()
-    ctx.moveTo(250, 150)
-    ctx.lineTo(550, 50)
-    ctx.stroke()
-
-    ctx.font = '30px Arial';
-    ctx.textAlign = 'center';
-
-    let color1 = 'grey'
-    let color2 = 'white'
-
-    if (mouseOverTitle) {
-        color1 = 'white'
-        color2 = 'grey'
-    }
-    else {
-        color1 = 'grey'
-        color2 = 'white'
-    }
-
-    ctx.strokeStyle = color1;
-    ctx.strokeText('Dodge.io', 320, 80);
-    
-    ctx.strokeStyle = color2;
-    ctx.strokeText('Fan Game', 470, 135);
-
-
-    // PLAY BUTTON //
-    const playGrad = ctx.createLinearGradient(250, 500, 550, 600)
-    const playGrad2 = ctx.createLinearGradient(250, 600, 550, 500)
-
-    mouseOverPlayButton = (mouseX > 250 && mouseX < 550) && (mouseY > 500 && mouseY < 600);
-
-    if (mouseOverPlayButton) {
-        playGrad.addColorStop(0, "rgb(0, 255, 0)");
-        playGrad.addColorStop(1, "rgb(255, 255, 255)");
-
-        playGrad2.addColorStop(0, "rgb(255, 255, 255)");
-        playGrad2.addColorStop(1, "rgb(0, 255, 0)");
-    } else {
-        playGrad.addColorStop(0, "rgb(255, 255, 255)");
-        playGrad.addColorStop(1, "rgb(0, 255, 0)");
-
-        playGrad2.addColorStop(0, "rgb(0, 255, 0)");
-        playGrad2.addColorStop(1, "rgb(255, 255, 255)");
-    }
-
-    ctx.fillStyle = playGrad;
-    ctx.fillRect(250, 500, 300, 100)
-
-    ctx.strokeStyle = playGrad2;
-    ctx.beginPath()
-    ctx.moveTo(250, 600)
-    ctx.lineTo(550, 500)
-    ctx.stroke()
-
-
-    let color3 = 'lime'
-    let color4 = 'white'
-
-    if (mouseOverPlayButton) {
-        color3 = 'white'
-        color4 = 'lime'
-    }
-    else {
-        color3 = 'lime'
-        color4 = 'white'
-    }
-    
-    ctx.strokeStyle = color3;
-    ctx.strokeText('Press', 320, 530);
-    
-    ctx.strokeStyle = color4;
-    ctx.strokeText('Play', 470, 585);
-}
-
-function drawPlayer() {
-    ctx.fillStyle = "rgb(255, 0, 0)"
-    ctx.beginPath()
-    ctx.arc(player.x, player.y, player.radius, Math.PI*2, 0)
-    ctx.fill()
-}
-
-function drawEnemies() {
-    ctx.fillStyle = "rgb(100, 100, 100)"
-    allEnemies.forEach(enemy => {
-        ctx.beginPath()
-        ctx.arc(enemy["x"], enemy["y"], enemy["radius"], Math.PI*2, 0)
-        ctx.fill()
-    })
-}
-
-function drawScore() {
-    ctx.font = '20px Arial';
-    ctx.textAlign = 'center';
-        
-    ctx.fillStyle = "rgb(87, 87, 87)";
-    ctx.fillText(`Score: ${score}`, 200, 40);
-    ctx.fillText(`Enemy Count: ${allEnemies.length}`, 600, 40);
-
-    ctx.fillStyle = highscoreColor;
-    ctx.fillText(`Highscore: ${highscore}`, 400, 40);
-}
-
-function spawnEnemyPeriodically() {
-    // Increment time and score
-    time++;
-    score = Math.round(time/10);
-    if (Number(score) > Number(highscore)) {
-        highscore = score;
-        highscoreColor = "red";
-    }
-
-    // Spawn enemies after a grace period and then spawn them periodically
-    if (allEnemies.length < 100 && time > 500) {
-        if (time % enemySpawnTime == 0) {
-            allEnemies.push(createEnemy());
-        
-
-            if (allEnemies.length % 10 == 0) {
-                enemySpawnTime -= 20;
-            }
-        }
-    }
-}
-
-function keyboardControls() {
-    player.speed = 2.5 * shiftPressed;
-    let dx = 0;
-    let dy = 0;
-
-    if (wPressed) dy -= 1;
-    if (sPressed) dy += 1;
-    if (aPressed) dx -= 1;
-    if (dPressed) dx += 1;
-
-    // Normalize diagonal movement
-    if (dx !== 0 && dy !== 0) {
-        const scale = Math.SQRT1_2; // 1 / √2 ≈ 0.7071
-        dx *= scale;
-        dy *= scale;
-    }
-
-    player.x += dx * player.speed;
-    player.y += dy * player.speed;
-
-    // Doesnt allow the player to leave the map
-    if (player.x - player.radius  <= 0 || player.x + player.radius  >= cnv.width) player.x -= dx * player.speed;
-    if (player.y - player.radius  <= 0 || player.y + player.radius  >= cnv.height) player.y -= dy * player.speed;
-}
-
-function mouseMovement() {
-    if (mouseMovementOn) {
-        const dx = mouseX - player.x;
-        const dy = mouseY - player.y;
-        const distance = Math.hypot(dx, dy);
-        player.speed = 2.5 * shiftPressed;
-        if (distance > 5) {
-            player.x += (dx / distance) * player.speed;
-            player.y += (dy / distance) * player.speed;
-        }
-
-        // Doesnt allow the player to leave the map
-        if (player.x - player.radius  <= 0 || player.x + player.radius  >= cnv.width) player.x -= (dx / distance) * player.speed;
-        if (player.y - player.radius  <= 0 || player.y + player.radius  >= cnv.height) player.y -= (dy / distance) * player.speed;
-    }
-}
-
-function moveEnemies() {
-    allEnemies.forEach(enemy => {
-        enemy["x"] += enemy["movex"]
-        enemy["y"] += enemy["movey"]
-        
-        // Doesnt allow the enemies to leave the map
-        if (enemy["x"] - enemy["radius"]  <= 0 || enemy["x"] + enemy["radius"]  >= cnv.width) enemy["movex"] *= -1
-        if (enemy["y"] - enemy["radius"]  <= 0 || enemy["y"] + enemy["radius"]  >= cnv.height) enemy["movey"] *= -1
-    })
-
-}
-
-function createEnemy() {
-    let oneEnemy = {
-        x: (Math.random() * (cnv.width-60))+30,
-        y: (Math.random() * (cnv.height-60))+30,
-        radius: (Math.random() * 10) + 10,
-        speed: Math.random() + 0.3,
-    }
-
-
-    let dx = player.x - oneEnemy.x;
-    let dy = player.y - oneEnemy.y;
-    let distance = Math.hypot(dx, dy);
-
-    while(distance < 300) {
-        oneEnemy.x = (Math.random() * (cnv.width-60))+30;
-        oneEnemy.y = (Math.random() * (cnv.height-60))+30;
-
-        dx = player.x - oneEnemy.x;
-        dy = player.y - oneEnemy.y;
-        distance = Math.hypot(dx, dy);
-    }
-
-    oneEnemy.movex = (dx / distance) * oneEnemy.speed;
-    oneEnemy.movey = (dy / distance) * oneEnemy.speed;
-
-    return oneEnemy;
-}
-
-function collisions() {
-
-    allEnemies.forEach(enemy => {
-        const dx = player.x - enemy.x;
-        const dy = player.y - enemy.y;
-        const distance = Math.hypot(dx, dy);
-
-        if (distance < player.radius + enemy.radius) {
-            highscoreColor = "rgb(87, 87, 87)";
-            gameState = "gameOver"
-        }
-    });
-}
-
-function drawGameOver() {
-    // RESTART BUTTON //
-    const grad = ctx.createLinearGradient(250, 50, 550, 150)
-    const grad2 = ctx.createLinearGradient(250, 150, 550, 50)
-
-    mouseOverRestartButton = (mouseX > 250 && mouseX < 550) && (mouseY > 50 && mouseY < 150);
-
-    if (mouseOverRestartButton) {
-        grad.addColorStop(0, "rgb(255, 0, 0)");
-        grad.addColorStop(1, "rgb(255, 255, 255)");
-
-        grad2.addColorStop(0, "rgb(255, 255, 255)");
-        grad2.addColorStop(1, "rgb(255, 0, 0)");
-    } else {
-        grad.addColorStop(0, "rgb(255, 255, 255)");
-        grad.addColorStop(1, "rgb(255, 0, 0)");
-
-        grad2.addColorStop(0, "rgb(255, 0, 0)");
-        grad2.addColorStop(1, "rgb(255, 255, 255)");
-    }
-
-    ctx.fillStyle = grad;
-    ctx.fillRect(250, 50, 300, 100)
-
-    ctx.strokeStyle = grad2;
-    ctx.beginPath()
-    ctx.moveTo(250, 150)
-    ctx.lineTo(550, 50)
-    ctx.stroke()
-
-    // Text
-    ctx.font = '30px Arial';
-    ctx.textAlign = 'center';
-
-    let gameOverColor = 'red'
-    let tryAgainColor = 'white'
-    if (mouseOverRestartButton) {
-        gameOverColor = 'white'
-        tryAgainColor = 'red'
-    }
-    else {
-        gameOverColor = 'red'
-        tryAgainColor = 'white'
-    }
-    
-    ctx.strokeStyle = gameOverColor;
-    ctx.strokeText('Game Over', 335, 80);
-
-    ctx.strokeStyle = tryAgainColor;
-    ctx.strokeText('Try Again', 480, 135);
-}
-
-function restartGame() {
-    mouseMovementOn = false;
-    
-    allEnemies = []
-    for(let i = 0; i < 10; i++) {
-        allEnemies.push(createEnemy());
-    }
-
-    time = 0;
-    score = 0;
-    enemySpawnTime = 200;
-    gameState = "gameOn"
-}

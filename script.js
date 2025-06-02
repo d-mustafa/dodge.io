3fc6fdf6be2d756d0a07e60b5d320e0e1d62e763
Divine <160066288+d-mustafa@users.noreply.github.com>
2025-06-01 22:27:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // DODGE.IO
-console.log("fuckin... font size... and shi")
+console.log("fuckin... background color changes... and shi")
 const cnv = document.getElementById("canvas");
 const ctx = cnv.getContext('2d');
 let gameState = "startScreen";

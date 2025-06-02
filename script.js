651fcb008d3569776b065b0ee3d7882164ecdfde
Divine <160066288+d-mustafa@users.noreply.github.com>
2025-06-02 15:52:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // DODGE.IO
-console.log("first class")
+console.log("enemy spawn rate")
 const cnv = document.getElementById("canvas");
 const ctx = cnv.getContext('2d');
 let gameState = "startScreen";

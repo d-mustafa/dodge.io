c07c26e2a3dc45a1355e34136dbf9a780cb3e18a
Divine <160066288+d-mustafa@users.noreply.github.com>
2025-06-01 21:35:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // DODGE.IO
-console.log("dodger boxes (evenly spaces)")
+console.log("start screen button locations swap")
 const cnv = document.getElementById("canvas");
 const ctx = cnv.getContext('2d');
 let gameState = "startScreen";

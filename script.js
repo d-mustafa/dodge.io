3cc2ffc0322aa9ff99c9479dd45080ed118630bc
Divine <160066288+d-mustafa@users.noreply.github.com>
2025-06-02 18:56:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // DODGE.IO
-console.log("color changes")
+console.log("color changes  3")
 const cnv = document.getElementById("canvas");
 const ctx = cnv.getContext('2d');
 let gameState = "startScreen";

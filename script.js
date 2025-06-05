bfa32d499238e6b16de713f52a30ddb155ddf0da
Divine <160066288+d-mustafa@users.noreply.github.com>
2025-06-04 20:18:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // DODGE.IO
-console.log("filtering and concatenation attempt 2")
+console.log("filtering and concatenation new enemies")
 const cnv = document.getElementById("canvas");
 const ctx = cnv.getContext('2d');
 let gameState = "startScreen";

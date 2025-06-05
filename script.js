fc0d93f7fedadd39aba03aa02075a2d9cabe9902
Divine <160066288+d-mustafa@users.noreply.github.com>
2025-06-04 18:05:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // DODGE.IO
-console.log("stronger adjustment")
+console.log("stronger adjustment + stronger aura")
 const cnv = document.getElementById("canvas");
 const ctx = cnv.getContext('2d');
 let gameState = "startScreen";

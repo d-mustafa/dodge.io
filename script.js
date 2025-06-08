bfb818f5418e18d31b206d7fea0a0d864fd9ba9b
Divine <160066288+d-mustafa@users.noreply.github.com>
2025-06-08 17:43:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // DODGE.IO
-console.log("window.addEventListener('mousemove', handler)")
+console.log("boudingCLient")
 const cnv = document.getElementById("canvas");
 const ctx = cnv.getContext('2d');
 let gameState = "startScreen";

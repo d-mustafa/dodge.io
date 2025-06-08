aafd0f32c7a11239df981d3745c75c59a2d5ad49
Divine <160066288+d-mustafa@users.noreply.github.com>
2025-06-08 11:09:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // DODGE.IO
-console.log("PageX, PageY. Also difficulty bug")
+console.log("PageXY")
 const cnv = document.getElementById("canvas");
 const ctx = cnv.getContext('2d');
 let gameState = "startScreen";

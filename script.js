f98987f27fc98c7a530c995858a042f486f26662
Divine <160066288+d-mustafa@users.noreply.github.com>
2025-06-08 22:55:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -138,7 +138,7 @@ function draw() {
         ctx.textAlign = "center";
         ctx.fillText(options[LI], cnv.width/2, cnv.height/2);
     }
-    else if (now - loadingGame > 5000 && gameState != "startScreen") gameState = "startScreen";
+    else if (now - loadingGame > 5000 && gameState == "loading") gameState = "startScreen";
 
     // Actual Game
     if (gameState == "startScreen") {

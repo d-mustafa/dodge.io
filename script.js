2498dd62f119098f4e48ad87b650725e0a952196
Divine <160066288+d-mustafa@users.noreply.github.com>
2025-06-08 19:44:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // DODGE.IO
-console.log("typos");
+console.log("typos and corrupted data check");
 const cnv = document.getElementById("canvas");
 const ctx = cnv.getContext('2d');
 let gameState = "startScreen";

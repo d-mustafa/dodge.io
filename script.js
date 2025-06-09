a03728173a0bc37e0cb5cc98b88d28a60b7f453a
Divine <160066288+d-mustafa@users.noreply.github.com>
2025-06-08 22:46:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // DODGE.IO
-console.log("difficulty color");
+console.log("startscreen");
 const cnv = document.getElementById("canvas");
 const ctx = cnv.getContext('2d');
 let gameState = "loading";

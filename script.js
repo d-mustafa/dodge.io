48b129bc76cf3ea755114e1a23e1423a8432460e
Divine <160066288+d-mustafa@users.noreply.github.com>
2025-06-13 21:31:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // DODGE.IO
-console.log("facing angle and loading screen bugs x44");
+console.log("facing angle and loading screen bugs x100");
 const cnv = document.getElementById("canvas");
 const ctx = cnv.getContext('2d');
 

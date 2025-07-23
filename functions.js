2dd8bf52fa588b56116425bfd261cd4716730b37
Divine <160066288+d-mustafa@users.noreply.github.com>
2025-07-23 17:48:14 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -143,7 +143,7 @@ function recordLeftClick() {
         })
 
         ["alarm9"].forEach(level => {
-            if (mouserOver[level]) {
+            if (mouseOver[level]) {
                 if (mouseOver.alarm9) music = {name: "Alarm 9", artist: "Blue Cxve", duration: alarm9.duration, color: "rgb(163, 0, 163)"};
                 innerGameState = 'inMusicMode';
                 mouseMovementOn = previousMM;

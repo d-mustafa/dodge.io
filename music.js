4e1292113e739b06d1b2240c8ba0ae17af6625ef
Divine <160066288+d-mustafa@users.noreply.github.com>
2025-07-23 15:59:57 -0600

    Update music.js

--- music.js
+++ music.js
@@ -2,8 +2,16 @@
 console.log("music")
 function restartMusicMode() {
   alarmNine.currentTime = 0;
-  if (difficulty.level === "alarmNine") alarmNine.play();
-  
+  if (difficulty.level === "alarmNine") {
+    alarmNine.play();
+    musicDuration = alarmNine.duration;
+  }
+
+  startTime = Date.now();
+  currentTime = (now-startTime) / 1000;
+  timeLeft = (musicDuration - currentTime).toFixed(2);
+  dash.lastEnded = 0;
+  shockwave.lastEnded = 0;
   gameState = "musicMode";
 }
 

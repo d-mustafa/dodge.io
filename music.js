c6ec9263572e3f2b16b98460e5b82cc876e82e77
Divine <160066288+d-mustafa@users.noreply.github.com>
2025-07-24 08:33:07 -0600

    Update music.js

--- music.js
+++ music.js
@@ -1,25 +1,15 @@
-console.log("alarm9 volume");// DODGE.IO - JSAB.JS
+console.log("removed the normal currentTime and startTime");// DODGE.IO - JSAB.JS
 function restartMusicMode() {
-  volume = Math.floor((settings.volumeSliderX - 165) / 1.5);
-  if (music.name === "Alarm 9") {
-    alarm9.currentTime = 0;
-    alarm9.volume = volume/100;
-    music.promise = alarm9.play();
-    music.duration = alarm9.duration;
-  }
-
-  startTime = Date.now();
-  currentTime = ((now-startTime) / 1000).toFixed(2);
-  timeLeft = (music.duration - currentTime).toFixed(2);
+  timeLeft = (music.duration - music.currentTime).toFixed(2);
   dash.lastEnded = 0;
   shockwave.lastEnded = 0;
   gameState = "musicMode";
 }
 
-function pauseAudio(audio) { // Pause music without causing errors
+function pauseAudio() { // Pause music without causing errors
   if (music.promise !== undefined) {
     music.promise.then(_ => {
-      audio.pause();
+      music.var.pause();
     })
     .catch(error => {
       console.warn(error);

36484f3bfa5bc3849505d34571de1c92a981a6c6
Divine <160066288+d-mustafa@users.noreply.github.com>
2025-07-24 10:41:53 -0600

    Update music.js

--- music.js
+++ music.js
@@ -1,4 +1,4 @@
-console.log("restartMusicMode");// DODGE.IO - JSAB.JS
+console.log("drawEndLevel");// DODGE.IO - JSAB.JS
 function restartMusicMode() {
   volume = Math.floor((settings.volumeSliderX - 165) / 1.5);
   music.var.volume = volume/100;
@@ -19,6 +19,23 @@ function pauseAudio() { // Pause music without causing errors
     .catch(error => {
       console.warn(error);
     });
+  }
+}
+
+function drawEndLevel() {
+  if (timeLeft <= 0) {
+    ctx.fillStyle = "rgb(0, 255, 0)";
+    ctx.fillRect(cnv.width/2, cnv.height/2, 100, 100);
+
+    dist = Math.hypot(player.x - cnv.width/2, player.y - cnv.height/2);
+
+    if (dist + player.radius <= 100) {
+
+    }
+
+    if (player.x + player.radius < cnv.width/2 + 100) {
+      
+    }
   }
 }
 

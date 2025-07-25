03a5a0cab88df2a72f83dbaad0ec3362f880cdb1
Divine <160066288+d-mustafa@users.noreply.github.com>
2025-07-25 11:53:53 -0600

    Update music.js

--- music.js
+++ music.js
@@ -1,4 +1,4 @@
-console.log("restart and exit button, growthfactor");// DODGE.IO - MUSIC.JS
+console.log("growthfactor and layers");// DODGE.IO - MUSIC.JS
 function restartMusicMode() {
     allEnemies = [];
     player.lives = 3;

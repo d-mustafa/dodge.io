5a146574c2b0bb6cdd5e596a0d7a99771fc24f6e
Divine <160066288+d-mustafa@users.noreply.github.com>
2025-07-24 21:51:57 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-console.log("draw player lives")// DODGE.IO - FUNCTIONS.JS
+console.log("alarm9 timestamps (accurate)")// DODGE.IO - FUNCTIONS.JS
 // KEYBAORD AND MOUSE EVENTS (player inputs)
 function recordKeyDown(event) {
     // stops the page from scrolling when arrow keys are pressed
